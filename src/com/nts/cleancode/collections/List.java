b431d426427d7f4fca79e3fdc6d38ab15b74b972
changuk63 <changuk63@naver.com>
2016-06-08 16:11:26 +0900

    modify method add in List class

--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -10,17 +10,12 @@ public class List extends AbstractCollection {
 	}
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + 10];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
-			}
-			elements[size++] = element;
-		}
+		if (readOnly)
+			return;
+		if (shouldGrow())
+			grow();
+		
+		addElements(element);
 	}
 	
 	private boolean shouldGrow(){
