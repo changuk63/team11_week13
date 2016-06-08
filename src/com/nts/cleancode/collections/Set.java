33db68a344c97ce048a1ce62bad4412b811a170b
changuk63 <changuk63@naver.com>
2016-06-08 19:56:14 +0900

    pull down size to classMap

--- src/com/nts/cleancode/collections/Set.java
+++ src/com/nts/cleancode/collections/Set.java
@@ -1,5 +1,10 @@
 package com.nts.cleancode.collections;
 
 public class Set extends AbstractCollection {
-	private static int INITIAL_CAPACITY = 10;
+	public void add(Object element){
+		if(contains(element))
+			return;
+		super.add(element);
+	}
+	
 }
